e53d78ef2b7d8fc14c78e288c4a358284bf13e4b
Romdik <romdik83@gmail.com>
2025-06-29 16:21:23 +0300

    Game2

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,37 +1,34 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
 
-    ArrayList<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = findByName(playerName1);
-        Player player2 = findByName(playerName2);
+        Integer player1 = findByName(playerName1);
+        Integer player2 = findByName(playerName2);
         if (player1 == null) {
             throw new NotRegisteredException(playerName1);
         }
         if (player2 == null) {
             throw new NotRegisteredException(playerName2);
         }
-        if (player1.getStrength() > player2.getStrength()) {
+        if (player1 > player2) {
             return 1;
         }
-        if (player1.getStrength() < player2.getStrength()) {
+        if (player1 < player2) {
             return 2;
         }
         return 0;
     }
 
-    public Player findByName(String name) {
-        for (Player player : players) {
-            if (player.getName().equals(name)) {
-                return player;
-            }
+    public Integer findByName(String name) {
+        if (players.get(name) != null) {
+            return players.get(name);
         }
         return null;
     }
